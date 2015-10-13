088c8c30732ce80d416ff867574b5056beaea158
dklynn <dk@datakoncepts.com>
2015-10-13 16:04:12 +1300

    Update configure.php
    Updated to allow extension match with ext_array and excl_array using lowercase.
    
    Updated to allow/forbid extensionless filenames.

--- configure.php
+++ configure.php
@@ -14,12 +14,12 @@ if ($_SERVER['REMOTE_ADDR']=='127.0.0.1')
 //	***	define("SCAN_PATH", '{local Virtual Host}');
 	define("SCAN_PATH", 'X:/path/to/account/');
 //	*** define("SCANNER_PATH", '{local path to}'.'/scandb.php}');
-	define("SCANNER_PATH", 'X:/path/to/superscan/');
+	//	define("SCANNER_PATH", 'X:/path/to/superscan/');
 } else {
 	//	For security, set SCAN_PATH inside your webspace
 	define("SCAN_PATH", "/home/account/public_html/");
 	// 	and SCANNER_PATH outside (NOT in public_html)	
-	define("SCANNER_PATH", "/home/account/superscan/");
+	//	define("SCANNER_PATH", "/home/account/superscan/");
 }
 
 
@@ -30,6 +30,10 @@ $testing = false;
 
 //	SET Report Output
 
+//	Output to monitor (true or false)
+//		Recommend true for testing and FALSE for CRON
+$report_out = false;
+
 //	Output as e-mail (true or false)
 //		Recommend false for testing and true for CRON
 $email_out = false;
@@ -37,24 +41,28 @@ $email_out = false;
 //	E-mail address(es) to send reports of change
 $addresses = array("user1@domain1.com", "user2@domain2.com");
 
-//	Output to monitor (true or false)
-//		Recommend true for testing and FALSE for CRON
-$report_out = false;
 
 //	Extensions to fetch
 //  	Example: $ext = array("php", "html", "htm", "js");   
 //	Recommended: An empty array will return ALL extensions 
 //		which is best for real security
-$ext = array();
+$ext_array = array();
+//	Make extensions lower case for scanner comparison
+$ext_array = array_map('strtolower',$ext_array);
 
 // 	extensions to omit
 //		An empty array will return all extensions
 //      *** The $excl_ext array can only contain elements *** 
-//		  *** if $ext array above is empty *** 
+//		*** if $ext array above is empty *** 
 $excl_ext = array('ftpquota','txt');
+//	Make extensions lower case for scanner comparison
+$excl_array = array_map('strtolower',$excl_array);
+
+//	Scan extensionless files?
+$extensionless = false;
 
 // 	directories to ignore
-//		An empty array will check all directories
+//		An empty array will check all directories in the SCAN_PATH tree
 $skip = array("protected", "private");
 
 
